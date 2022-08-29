85ba197b75e7ed8445809476cff7599c7732f61a
sudheshtnamupparapu1999@gmail.com <sudheshtnamupparapu1999@gmail.com>
2022-08-29 19:23:41 +0530

    Added: week

---
+++ week.sh
@@ -0,0 +1,25 @@
+read -p "enter number:" character
+if(( character == 1 ))
+then
+echo "sunday"
+elif(( character == 2 ))
+then
+echo "monday"
+elif(( character == 3 ))
+then
+echo "tuesday"
+elif(( character == 4 ))
+then
+echo "wednesday"
+elif(( character == 5 ))
+then
+echo "thursday"
+elif(( character == 6 ))
+then
+echo "friday"
+elif(( character ==7 ))
+then
+echo "saturday"
+else
+echo "invalid day"
+fi
