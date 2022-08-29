2d62656cd6332a4a7e8cdbd7c9a6f39b655b9291
sudheshtnamupparapu1999@gmail.com <sudheshtnamupparapu1999@gmail.com>
2022-08-29 21:27:20 +0530

    Added: character

---
+++ character.sh
@@ -0,0 +1,19 @@
+read -p "enter number:" character
+if(( character == 1 ))
+then 
+echo " one "
+elif(( character == 2 ))
+then
+echo " two "
+elif(( character == 3 ))
+then 
+echo " three "
+elif(( character == 4 ))
+then
+echo " four "
+elif(( character == 5 ))
+then
+echo " five "
+else
+echo " this is not a single digit "
+fi
